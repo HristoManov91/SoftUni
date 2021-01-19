da9968fec4c9c8be6a2eab3ee3210c6f5f3a670e
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2021-01-19 12:05:31 +0200

    add tasks from Polymorphism - Lab

---
+++ JavaOOP/05.Polymorphism/Shapes/Rectangle.java
@@ -0,0 +1,29 @@
+package Shapes;
+
+public class Rectangle extends Shape {
+    private final Double height;
+    private final Double width;
+
+    public Rectangle(Double height, Double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
+
+    @Override
+    public Double calculatePerimeter() {
+        return 2 * this.height + 2 * this.width;
+    }
+
+    @Override
+    public Double calculateArea() {
+        return this.height * this.width;
+    }
+}
