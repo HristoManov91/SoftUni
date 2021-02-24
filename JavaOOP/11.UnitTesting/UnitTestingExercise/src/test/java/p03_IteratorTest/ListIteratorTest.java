b65182588b366ce4b958845371992427c9ebcec7
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2021-02-24 20:57:42 +0200

    add task

---
+++ JavaOOP/11.UnitTesting/UnitTestingExercise/src/test/java/p03_IteratorTest/ListIteratorTest.java
@@ -0,0 +1,56 @@
+package p03_IteratorTest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.naming.OperationNotSupportedException;
+
+import static org.junit.Assert.*;
+
+public class ListIteratorTest {
+    private ListIterator listIterator;
+    private static final String[] DATA = {"X", "Y", "Z"};
+
+    @Before
+    public void setUp() throws OperationNotSupportedException {
+    listIterator = new ListIterator(DATA);
+}
+
+    @Test (expected = OperationNotSupportedException.class)
+    public void testConstructorWithNullElements() throws OperationNotSupportedException {
+        new ListIterator(null);
+    }
+
+    @Test
+    public void testHasNextReturnCorrectBoolean(){
+        Assert.assertTrue(listIterator.hasNext());
+        listIterator.move();
+        Assert.assertTrue(listIterator.hasNext());
+        listIterator.move();
+        Assert.assertFalse(listIterator.hasNext());
+    }
+
+    @Test
+    public void testMove(){
+        Assert.assertTrue(listIterator.move());
+        Assert.assertTrue(listIterator.move());
+        Assert.assertFalse(listIterator.move());
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void testPrintEmptyList() throws OperationNotSupportedException {
+        ListIterator listIterator = new ListIterator();
+        listIterator.print();
+    }
+
+    @Test
+    public void testPrintCorrectly(){
+        int index = 0;
+        while (listIterator.hasNext()){
+            Assert.assertEquals(DATA[index] , listIterator.print());
+            index++;
+            listIterator.move();
+        }
+    }
+}
