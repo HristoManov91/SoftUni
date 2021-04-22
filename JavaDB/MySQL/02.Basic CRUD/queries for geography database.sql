b8d48532542db38c6c943a333db81bde84f41cc0
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2021-04-22 23:42:02 +0300

    add tasks from Lab

--- JavaDB/MySQL/02.Basic CRUD/queries for geography database.sql
+++ JavaDB/MySQL/02.Basic CRUD/queries for geography database.sql
@@ -5,11 +5,11 @@ ORDER BY `peak_name`;
 #22
 SELECT `country_name` , `population` FROM `countries`
 WHERE `continent_code` = 'EU'
-ORDER BY `population` DESC
+ORDER BY `population` DESC, `country_name`
 LIMIT 30;
 
 #23
-SELECT `country_name` , `country_code` , `currency_code` FROM `countries`
+SELECT `country_name` , `country_code` , IF (`currency_code` = 'EUR' , 'Euro' , 'Not Euro') AS `currency` FROM `countries`
 ORDER BY `country_name`;
 
 #24
