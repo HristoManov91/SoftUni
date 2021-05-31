1bf89638d8dec10fe0fc15ac3dd691868b54cc4d
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2021-05-31 22:37:27 +0300

    add queries

--- JavaDB/MySQL/03.Built in Functions/queries from Exercise.sql
+++ JavaDB/MySQL/03.Built in Functions/queries from Exercise.sql
@@ -8,9 +8,81 @@ WHERE `last_name` LIKE '%ei%';
 
 #3
 SELECT `first_name` FROM `employees`
-WHERE department_id IN (3 , 10) AND YEAR(`hire_date`) BETWEEN 1995 AND 2005
+WHERE `department_id` IN (3 , 10) AND YEAR(`hire_date`) BETWEEN 1995 AND 2005
 ORDER BY `employee_id`;
 
 #4
 SELECT `first_name` , `last_name` FROM `employees`
 WHERE `job_title` NOT LIKE '%engineer%';
+
+#5
+SELECT `name` FROM `towns`
+WHERE CHAR_LENGTH(`name`) IN (5,6)
+ORDER BY `name`;
+
+#6
+SELECT * FROM `towns`
+WHERE LEFT(`name` , 1) IN ('M' , 'K' , 'B' , 'E')
+ORDER BY `name`;
+
+#7
+SELECT * FROM `towns`
+WHERE LEFT(`name` , 1) NOT IN ('R' , 'B' , 'D' )
+ORDER BY `name`;
+
+#8
+CREATE VIEW v_employees_hired_after_2000 AS
+SELECT `first_name` , `last_name` FROM `employees`
+WHERE YEAR(`hire_date`) > 2000;
+
+SELECT v_employees_hired_after_2000;
+
+#9
+SELECT `first_name` , `last_name` FROM `employees`
+WHERE CHAR_LENGTH(`last_name`) = 5;
+
+#10
+SELECT `country_name` , `iso_code` FROM countries
+WHERE `country_name` LIKE '%a%a%a%'
+ORDER BY `iso_code`;
+
+#11
+SELECT `peak_name` , `river_name` , LOWER(CONCAT(`peak_name` , SUBSTRING(`river_name` , 2))) AS 'mix' FROM `peaks` , `rivers`
+WHERE RIGHT(LOWER(`peak_name`) , 1) = LEFT(LOWER(`river_name`) , 1)
+ORDER BY `mix`;
+
+#12
+SELECT `name` , DATE_FORMAT(`start` , '%Y-%m-%d') AS `start` FROM `games`
+WHERE YEAR(`start`) IN (2011 , 2012)
+ORDER BY `start` , `name`
+LIMIT 50;
+
+#13
+SELECT `user_name` , SUBSTRING(`email` , LOCATE('@' , `email`) + 1)  AS 'Email Provider' FROM `users`
+ORDER BY `Email Provider` , `user_name`;
+
+#14
+SELECT `user_name` , `ip_address` FROM `users`
+WHERE `ip_address` LIKE '___.1%.%.___'
+ORDER BY `user_name`;
+
+#15
+SELECT `name` AS 'game' ,
+	(CASE
+		WHEN HOUR(`start`) BETWEEN 0 AND 11 THEN 'Morning'
+		WHEN HOUR(`start`) BETWEEN 12 AND 17 THEN 'Afternoon'
+		WHEN HOUR(`start`) BETWEEN 18 AND 23 THEN 'Evening'
+	END) AS 'Part of the Day' ,
+    (CASE
+		WHEN `duration` <= 3 THEN 'Extra Short'
+		WHEN `duration` BETWEEN 4 AND 6 THEN 'Short'
+		WHEN `duration` BETWEEN 7 AND 10 THEN 'Long'
+		ELSE 'Extra Long'
+    END) AS 'Duration'
+FROM `games`;
+
+#16
+SELECT `product_name` , `order_date` , 
+	DATE_ADD(`order_date` , INTERVAL 3 DAY) AS 'pay_due' , 
+    DATE_ADD(`order_date`, INTERVAL 1 MONTH) AS 'deliver_due' 
+FROM `orders`;
