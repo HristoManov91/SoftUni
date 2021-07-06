f93c1e03c185c079933ac444423f5a47bfb103b7
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2021-07-06 21:14:50 +0300

    add

---
+++ JavaDB/SpringData/04.Hibernate Code First/HibernateCodeFirst-Exercise/src/main/java/entity/Teacher.java
@@ -0,0 +1,35 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table (name = "teachers")
+public class Teacher extends User {
+
+    private BigDecimal price;
+    private String model;
+
+    public Teacher() {
+    }
+
+    @Column (name = "price")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Column (name = "model")
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
