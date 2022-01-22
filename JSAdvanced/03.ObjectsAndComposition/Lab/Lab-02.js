154cd2f4233d88022094c3523d4396f64384be4a
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2022-01-22 22:47:37 +0200

    added

---
+++ JSAdvanced/03.ObjectsAndComposition/Lab/Lab-02.js
@@ -0,0 +1,29 @@
+function townPopulation(input){
+
+    let result = {};
+    for (const element of input) {
+        let [town , population] = element.split(' <-> ');
+        if (!result[town]){
+            result[town] = 0;
+        }
+
+        result[town] += Number(population);
+    }
+
+    Object.keys(result).forEach(key => {console.log(`${key} : ${result[key]}`)})
+
+}
+
+townPopulation(['Sofia <-> 1200000',
+    'Montana <-> 20000',
+    'New York <-> 10000000',
+    'Washington <-> 2345000',
+    'Las Vegas <-> 1000000']
+);
+
+townPopulation(['Istanbul <-> 100000',
+    'Honk Kong <-> 2100004',
+    'Jerusalem <-> 2352344',
+    'Mexico City <-> 23401925',
+    'Istanbul <-> 1000']
+);
